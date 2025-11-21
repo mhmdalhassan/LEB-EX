46bf703df745d8b04fdbfa74d53f542401d4606d
Mohammad Al Alhassan <mhmdalhassan66@gmail.com>
2025-11-21 14:53:52 +0200

    Initialize LEBEX project

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen text-3xl font-bold">
+      LEBEX Platform Initialized
+    </main>
+  );
+}
